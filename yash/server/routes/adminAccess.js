67d2ff20d1df4162619479bed75fa822af9bcacc
kinjal5104 <144837128+kinjal5104@users.noreply.github.com>
2024-06-27 20:16:54 +0530

    Delete yash directory

--- yash/server/routes/adminAccess.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const bodyParser=require('body-parser');
-const router = express.Router();
-
-router.post('/', async(req,res)=>{
-    res.render('adminAccess');
-})
-
-module.exports=router;
